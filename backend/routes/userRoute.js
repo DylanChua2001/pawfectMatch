d5feded7b409e390384ad6fe1034db09f7784392
raellusionz <146325019+raellusionz@users.noreply.github.com>
2024-05-22 22:51:12 +0800

    Update userRoute.js

--- backend/routes/userRoute.js
+++ backend/routes/userRoute.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const userController = require('../controllers/userController')
 
-//Route to get all users
+// Route to get all users
 router.get('/getAllUsers', userController.getAllUsersC)
 
 module.exports = router;

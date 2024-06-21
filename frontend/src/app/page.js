7defc4691d3d21895726cb811abcb19d40a33281
raellusionz <146325019+raellusionz@users.noreply.github.com>
2024-06-21 20:46:58 +0800

    Commit

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -1,103 +1,149 @@
-"use client"
-import Image from "next/image";
-import styles from "./page.module.css";
-import { ChakraProvider } from "@chakra-ui/react";
-import {useRouter} from "next/navigation";
+'use client'
+
+import {
+  Flex,
+  Box,
+  FormControl,
+  FormLabel,
+  Input,
+  Checkbox,
+  Stack,
+  HStack,
+  Button,
+  Heading,
+  Text,
+  Avatar,
+  useColorModeValue,
+} from '@chakra-ui/react'
+import { useRouter } from 'next/navigation';
+import Image from 'next/image';
+import Link from 'next/link';
+
 
 export default function SimpleCard() {
   const router = useRouter();
 
   return (
-    <ChakraProvider>
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          <button type="button" onClick={() => router.push('/login')}>
-          Go to Login
-          </button>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Flex
+      height="100vh"
+      alignItems="center"
+      justifyContent="center"
+      direction="column"
+    >
+      <HStack position="fixed" top="2%" zIndex="1" justifyContent="center" width="100%">
+        <Image src="/pawprints.png" alt="Image" width={50} height={50} />
+        <Heading fontSize={'240%'} fontFamily="Kaushan Script" fontStyle='italic'>PawfectMatch</Heading>
+      </HStack>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
+      <HStack position="fixed" top="2%" right="2%" zIndex="1">
+        <Avatar
+          borderRadius='full'
+          borderColor= "black"
+          src= "profileicon.png"
+          width="50" 
+          height="50"
         />
-      </div>
+      </HStack>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
+      <Stack 
+        align={'center'} 
+        spacing={6} 
+        mx={'auto'} 
+        maxW={'xl'} 
+        mt= {40}>
+        <Box
+          borderRadius="10px"
+          paddingTop="3%"
+          paddingBottom="3%"
+          paddingLeft="10%"
+          paddingRight="10%"
+          bg="rgba(255, 250, 245, 0.7)"
+          width={['90%', '70%', '50vw']}
+          minHeight={['70vh', '85vh', '85vh']}
         >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+          <Heading 
+            fontSize="180%"
+            fontFamily="'Lilita One', cursive"
+            fontWeight= "bold"
+            textAlign="center"
+            mb={10}>
+            Login
+          </Heading>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-    </ChakraProvider>
-  );
+          <Stack spacing={4}>
+          
+            <FormControl id="email">
+              <Input 
+                type="email" 
+                placeholder='Email' 
+                width= "100%"
+                padding= "10"
+                my= '10'
+                size= "100"
+                borderRadius= "5px"
+                borderColor= "#D9D9D9"
+                _focus={{ bg: "white", borderColor: "blue.400" }}
+              />
+            </FormControl>
+            <FormControl id="password">
+              <Input 
+                type="password" 
+                placeholder= "Password"
+                width= "100%"
+                padding= "10"
+                my= "10"
+                size= "100"
+                borderRadius= "5px"
+                borderColor= "#D9D9D9"
+                _focus={{ bg: "white", borderColor: "blue.400" }}
+              />
+            </FormControl>
+            <Stack>
+              <Stack
+                direction={{ base: 'column', sm: 'row' }}
+                align={'start'}
+                justify={'space-between'}
+              >
+                <Checkbox
+                  fontFamily="sans-serif"
+                  fontSize= "13">
+                  <Text fontFamily="sans-serif" fontSize= "13">
+                  Remember me
+                  </Text>
+                </Checkbox>
+                <Text 
+                  color="blue"
+                  fontFamily="sans-serif"
+                  fontSize= "13">
+                  <Link href= "forgot-password">
+                    Forgot password?
+                  </Link>
+                </Text>
+              </Stack>
+              <Button
+                onClick={() => router.push('/pages/selection')}
+                _hover={{ cursor: 'pointer' }}
+                alignItems='center'
+                width= "100%"
+                padding= "10"
+                my= "5"
+                borderRadius= "5px"
+                backgroundColor= "#F8D3A7"
+                textColor='black'>
+                Login
+              </Button>
+            </Stack>
+            <Stack alignItems='center' mt= "5">
+              <Text fontFamily="sans-serif" fontSize= "13">
+                Don't have an account? {" "}
+                <Link href="/signup" passHref>
+                  <button style={{ color: 'blue', background: 'none', border: 'none', cursor: 'pointer' }}>Sign up</button>
+                </Link>
+              </Text>
+            </Stack>
+          </Stack>
+        </Box>
+      </Stack>
+    </Flex>
+  )
 }

4842885f395f42bcb346d31d93d8bed694c635ec
DylanChua2001 <dylanchua2001dc@gmail.com>
2024-06-25 23:30:31 +0800

    updated fe packages

--- frontend/src/app/page.js
+++ frontend/src/app/page.js
@@ -12,6 +12,7 @@ import {
   Heading,
   Text,
   Avatar,
+  Link,
 } from '@chakra-ui/react'
 import { useRouter } from 'next/navigation';
 import Image from 'next/image';
@@ -44,102 +45,103 @@ export default function SimpleCard() {
           />
         </HStack>
 
-      <Stack 
-        align={'center'} 
-        mx={'auto'} 
-        maxW={'xl'} 
-        mt= "40px">
-        <Box
-          borderRadius="10px"
-          paddingLeft="8%"
-          paddingRight="8%"
-          bg="rgba(255, 250, 245, 0.7)"
-          width={['90vw', '70vw', '50vw']}
-          minHeight={['70vh', '85vh', '85vh']}
-        >
-          <Heading 
-            fontSize="180%"
-            fontFamily="'Lilita One', cursive"
-            fontWeight= "bold"
-            textAlign="center"
-            mt= "4%">
-            Login
-          </Heading>
+        <Stack
+          align={'center'}
+          mx={'auto'}
+          maxW={'xl'}
+          mt="40px">
+          <Box
+            borderRadius="10px"
+            paddingLeft="8%"
+            paddingRight="8%"
+            bg="rgba(255, 250, 245, 0.7)"
+            width={['90vw', '70vw', '50vw']}
+            minHeight={['70vh', '85vh', '85vh']}
+          >
+            <Heading
+              fontSize="180%"
+              fontFamily="'Lilita One', cursive"
+              fontWeight="bold"
+              textAlign="center"
+              mt="4%">
+              Login
+            </Heading>
 
-          <Stack spacing={4}>
-          
-            <FormControl id="email">
-              <Input 
-                type="email" 
-                placeholder='Email' 
-                width= "100%"
-                padding= "20px"
-                mt= "4%"
-                size= "100"
-                borderRadius= "5px"
-                borderColor= "#D9D9D9"
-                _focus={{ bg: "white", borderColor: "blue.400" }}
-              />
-            </FormControl>
-            <FormControl id="password">
-              <Input 
-                type="password" 
-                placeholder= "Password"
-                width= "100%"
-                padding= "20px"
-                mt= "2%"
-                size= "100"
-                borderRadius= "5px"
-                borderColor= "#D9D9D9"
-                _focus={{ bg: "white", borderColor: "blue.400" }}
-              />
-            </FormControl>
-            <Stack>
-              <Stack
-                direction={{ base: 'column', sm: 'row' }}
-                align={'start'}
-                justify={'space-between'}
-                mt="2%"
-              >
-                <Checkbox
-                  fontFamily="sans-serif"
-                  fontSize= "13">
-                  <Text fontFamily="sans-serif" fontSize= "13">
-                  Remember me
+            <Stack spacing={4}>
+
+              <FormControl id="email">
+                <Input
+                  type="email"
+                  placeholder='Email'
+                  width="100%"
+                  padding="20px"
+                  mt="4%"
+                  size="100"
+                  borderRadius="5px"
+                  borderColor="#D9D9D9"
+                  _focus={{ bg: "white", borderColor: "blue.400" }}
+                />
+              </FormControl>
+              <FormControl id="password">
+                <Input
+                  type="password"
+                  placeholder="Password"
+                  width="100%"
+                  padding="20px"
+                  mt="2%"
+                  size="100"
+                  borderRadius="5px"
+                  borderColor="#D9D9D9"
+                  _focus={{ bg: "white", borderColor: "blue.400" }}
+                />
+              </FormControl>
+              <Stack>
+                <Stack
+                  direction={{ base: 'column', sm: 'row' }}
+                  align={'start'}
+                  justify={'space-between'}
+                  mt="2%"
+                >
+                  <Checkbox
+                    fontFamily="sans-serif"
+                    fontSize="13">
+                    <Text fontFamily="sans-serif" fontSize="13">
+                      Remember me
+                    </Text>
+                  </Checkbox>
+                  <Text
+                    color="blue"
+                    fontFamily="sans-serif"
+                    fontSize="13">
+                    <Link href="forgot-password">
+                      Forgot password?
+                    </Link>
                   </Text>
-                </Checkbox>
-                <Text 
-                  color="blue"
-                  fontFamily="sans-serif"
-                  fontSize= "13">
-                  <Link href= "forgot-password">
-                    Forgot password?
+                </Stack>
+                <Button
+                  onClick={() => router.push('/pages/selection')}
+                  _hover={{ cursor: 'pointer' }}
+                  alignItems='center'
+                  width="100%"
+                  padding="30px"
+                  mt="3%"
+                  borderRadius="5px"
+                  backgroundColor="#F8D3A7"
+                  textColor='black'>
+                  Login
+                </Button>
+              </Stack>
+              <Stack alignItems='center' mt="3%" fontFamily="sans-serif" fontSize="13" >
+                <Text>Don't have an account? {' '}
+                  <Link href="/signup" color='blue' background='none' border='none' cursor='pointer' display="inline-block" passHref >
+                    Sign up
                   </Link>
                 </Text>
               </Stack>
-              <Button
-                onClick={() => router.push('/pages/selection')}
-                _hover={{ cursor: 'pointer' }}
-                alignItems='center'
-                width= "100%"
-                padding= "30px"
-                mt= "3%"
-                borderRadius= "5px"
-                backgroundColor= "#F8D3A7"
-                textColor='black'>
-                Login
-              </Button>
             </Stack>
-            <Stack alignItems='center'mt="3%" fontFamily="sans-serif" fontSize= "13" >
-              <Text>Don't have an account? {' '} 
-                <Link href="/signup" color= 'blue' background= 'none' border= 'none' cursor= 'pointer' display= "inline-block" passHref >
-                  Sign up
-                </Link>
-              </Text>
-            </Stack>
-          </Stack>
-        </Box>
-      </Stack>
-    </Flex>
+          </Box>
+        </Stack>
+      </Flex>
+    </>
   )
 }

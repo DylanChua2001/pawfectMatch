6d095300190bcc393be0c0f60062815643e28f36
jong515 <138593509+jong515@users.noreply.github.com>
2024-06-29 16:34:13 +0800

    feat: added add pets component

--- frontend/src/app/components/header.js
+++ frontend/src/app/components/header.js
@@ -58,6 +58,7 @@ const Header = () => {
             <MenuItem onClick={() => navigateTo('/pages/pets')}>Pets</MenuItem>
             <MenuItem onClick={() => navigateTo('/pages/training')}>Training Packages</MenuItem>
             <MenuItem onClick={() => navigateTo('/pages/profile')}>Profile</MenuItem>
+            <MenuItem onClick={() => navigateTo('/pages/addPets')}>Add Pets</MenuItem>
             {/* Add more MenuItems for additional pages */}
           </MenuList>
         </Menu>

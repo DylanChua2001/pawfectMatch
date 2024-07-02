3f048f6e4a3209383335b7634bee15181992497f
shyancham <143789750+shyancham@users.noreply.github.com>
2024-07-02 22:45:03 +0800

    Shyan (#18)
    * Deleted login folder, made default page login page
    
    * Created header.js component, as well as component folder, edited pets page to import header
    
    * removed checkbox
    
    * moves the pages into a pages folder
    
    * Edited the routing to the correct pages, created a filter component and wrapped layout.js in the chakra provider
    
    * removed background.css
    
    * added background to pets page, will do so for others after
    
    * fixed hydration error by wrapping it in <></>
    
    * created filter component, with a save change button that will save the size as a constant.
    
    * added age range filter slider component
    
    * added price range slider, edited margins etc
    
    * Added background and header to selection page. Added labels to filter
    
    * Added pet selection button
    
    * added function to close filter on save
    
    * Added pets image, created new petCard and petList component, added them to page
    
    * edited the formatting of the petcards and petlist to allow it all to squeeze into one line
    
    * merged with main
    
    * Added scrollability function to the pets page, and made some margin adjustments
    
    * adjusted the pet card text and background
    
    * added pet profile page with component and images
    
    * Created a tempdata folder and pets.json to import data, added some fake dummy data. Pets page will import from there instead of a constant
    
    * Removed petprofile specific page, and made usestate to display data of each page dynamically
    
    * Adjusted margins and made the back to list a button
    
    * Added like/favorite button, installed icon package for heart shape
    
    * Added routing menu to the avatar icon
    
    * edited the login and selection page
    
    * edited the pets page, filter and petCard and PetList
    
    * edited about us page
    
    * edited about us page
    
    * edited pets page
    
    ---------
    
    Co-authored-by: jong515 <138593509+jong515@users.noreply.github.com>
    Co-authored-by: shyancham <shyancham@users.noreply.github.com>

--- frontend/src/app/pages/about/page.js
+++ frontend/src/app/pages/about/page.js
@@ -1,5 +1,5 @@
 // pages/about.js
-import { Box } from '@chakra-ui/react';
+import { Box, Flex } from '@chakra-ui/react';
 import Header from "../../components/header";
 import AboutUs from '../../components/aboutus';
 
@@ -7,17 +7,22 @@ import AboutUs from '../../components/aboutus';
 const AboutPage = () => {
   return (
     <>
-    <Box
-      display="flex"
-      flexDirection="column"
-      minHeight="100vh"
-    >
-      <Header />
-      <Box flex="1" display="flex" alignItems="center" justifyContent="center">
-        <AboutUs />
+    <Flex direction="column" height="100vh">
+      <Box mt="60px">
+        <Header />
       </Box>
-    </Box>
+      <Flex justifyContent="center">
+        <Box  
+          maxW={["90%", "92%", "97%"]}
+          w="100%" 
+          maxHeight={["calc(100vh - 200px)", "calc(100vh - 150px)", "calc(100vh - 180px)"]}
+          >
+          <AboutUs/>
+        </Box>
+      </Flex>
+    </Flex>
     </>
+    
   );
 };
 

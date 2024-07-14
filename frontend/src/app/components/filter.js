ade65fbbae83aff1b38a0469abc19f9413619236
DylanChua2001 <134465251+DylanChua2001@users.noreply.github.com>
2024-07-15 00:03:42 +0800

    Integrated filter and search component (#35)
    * Deleted login folder, made default page login page
    
    * Created header.js component, as well as component folder, edited pets page to import header
    
    * removed checkbox
    
    * moves the pages into a pages folder
    
    * Edited the routing to the correct pages, created a filter component and wrapped layout.js in the chakra provider
    
    * removed background.css
    
    * added background to pets page, will do so for others after
    
    * fixed hydration error by wrapping it in <></>
    
    * LLM v2 (sql data + postgres data store)
    
    * fixed exposed postgres db
    
    * created filter component, with a save change button that will save the size as a constant.
    
    * added age range filter slider component
    
    * added price range slider, edited margins etc
    
    * Added background and header to selection page. Added labels to filter
    
    * Added pet selection button
    
    * added function to close filter on save
    
    * Added pets image, created new petCard and petList component, added them to page
    
    * edited the formatting of the petcards and petlist to allow it all to squeeze into one line
    
    * Added scrollability function to the pets page, and made some margin adjustments
    
    * adjusted the pet card text and background
    
    * added pet profile page with component and images
    
    * Created a tempdata folder and pets.json to import data, added some fake dummy data. Pets page will import from there instead of a constant
    
    * Removed petprofile specific page, and made usestate to display data of each page dynamically
    
    * Adjusted margins and made the back to list a button
    
    * Added like/favorite button, installed icon package for heart shape
    
    * Added routing menu to the avatar icon
    
    * added search function to pet list and installed more icon packages
    
    * added profile page component and page.
    
    FOR RAE: Added strip page and button
    
    * llm v2.0 (more user character focused)
    
    * fixed hydration error
    
    * adding chat websocket backend
    
    * updated fe packages
    
    * fe fix
    
    * fe changes
    
    * chatbot integration
    
    * chat fe, be and integration v1.0
    
    * frontend fixes
    
    * installed express-session
    
    * session (not fixed yet)
    
    * login authentication
    
    * cookies fixed
    
    * integrated profile page
    
    * integrated hlaf of the chat page
    
    * fixed integration of chat page partially
    
    * integrated login, profile, chat page
    
    * filter and pet list integrated
    
    ---------
    
    Co-authored-by: jong515 <138593509+jong515@users.noreply.github.com>
    Co-authored-by: shyancham <shyancham@users.noreply.github.com>

--- frontend/src/app/components/filter.js
+++ frontend/src/app/components/filter.js
@@ -1,69 +1,78 @@
-import React, { useState, useRef } from 'react'
+import React, { useState, useRef } from 'react';
 import {
   Box,
-  Slider,
-  SliderMark,
-  SliderTrack,
-  SliderFilledTrack,
-  SliderThumb,
   Menu,
   MenuButton,
   MenuList,
   Button,
   useDisclosure,
   useOutsideClick,
-  Text
-} from '@chakra-ui/react'
+  Text,
+  RangeSlider,
+  RangeSliderTrack,
+  RangeSliderFilledTrack,
+  RangeSliderThumb,
+  RangeSliderMark,
+  useToast
+} from '@chakra-ui/react';
+import axios from 'axios';
+import Cookie from 'js-cookie';
 
-const FilterMenu = ({ onSelectAge, onSelectSize, onSelectPrice, onSelectPet }) => {
-  const [ageValue, setAgeValue] = useState(0) // Default age value
-  const [sizeValue, setSizeValue] = useState(1) // Default size value
-  const [priceValue, setPriceValue] = useState(0) // Default price value
-  const [pet, setPet] = useState('Dog') // Default pet value
+const FilterMenu = ({
+  onSelectAge,
+  onSelectSize,
+  onSelectPrice,
+  onSelectPet,
+  ageMin,
+  ageMax,
+  priceMin,
+  priceMax,
+  selectedSize,
+  selectedPet
+}) => {
+  const [ageValue, setAgeValue] = useState([ageMin, ageMax]);
+  const [priceValue, setPriceValue] = useState([priceMin, priceMax]);
+  // const [pet, setPet] = useState('Dog');
 
-  const sizeRanges = ["Small", "Medium", "Large"] // Size options
-  const pets = ["Dog", "Cat", "Rabbit"] // Pet options
+  const sizeRanges = ["Small", "Medium", "Large"];
+  const pets = ["Dog", "Cat", "Rabbit"];
 
   const labelStyles = {
     mt: '2',
     ml: '-2.5',
     fontSize: 'sm',
-  }
+  };
 
   const handleAgeChange = (val) => {
-    setAgeValue(val)
-    onSelectAge(val) // Call onSelectAge prop with the selected age value
-  }
+    setAgeValue(val);
+    onSelectAge(val);
+  };
 
-  const handleSizeChange = (val) => {
-    setSizeValue(val)
-    onSelectSize(sizeRanges[val - 1]) // Pass the size label instead of the index
-  }
+  const handleSizeChange = (size) => {
+    onSelectSize(size);
+  };
 
   const handlePriceChange = (val) => {
-    setPriceValue(val)
-    onSelectPrice(val) // Call onSelectPrice prop with the selected price value
-  }
+    setPriceValue(val);
+    onSelectPrice(val);
+  };
 
   const handlePetChange = (selectedPet) => {
-    setPet(selectedPet)
-    onSelectPet(selectedPet) // Call onSelectPet prop with the selected pet
-  }
+    onSelectPet(selectedPet);
+  };
 
   return (
     <>
       <Box p={4}>
         <Box mt={-2} mb={4}>
-        <Box>
-          <Text >Pet</Text>
+          <Text>Pet</Text>
           <Box>
             {pets.map((petName) => (
               <Button
                 key={petName}
                 onClick={() => handlePetChange(petName)}
-                bg={pet === petName ? 'blue.500' : 'gray.200'}
-                color={pet === petName ? 'white' : 'black'}
-                // colorScheme="yellow"
+                bg={selectedPet === petName ? 'blue.500' : 'gray.200'}
+                color={selectedPet === petName ? 'white' : 'black'}
                 mr={4}
               >
                 {petName}
@@ -71,55 +80,29 @@ const FilterMenu = ({ onSelectAge, onSelectSize, onSelectPrice, onSelectPet }) =
             ))}
           </Box>
         </Box>
-          <Text mt={4} mb={8}>Size</Text>
-          <Box pr={8}>
-            <Slider
-              aria-label='size-slider'
-              min={1}
-              max={3}
-              step={1}
-              value={sizeValue}
-              onChange={handleSizeChange}
-              ml={4} // Add ml={4} for left padding
-              mr={4} // Add mr={4} for right padding
-              width="calc(100% - 40px)" // Adjust width for equal length
-            >
-              <SliderMark value={1} {...labelStyles}>
-                Small
-              </SliderMark>
-              <SliderMark value={2} {...labelStyles}>
-                Medium
-              </SliderMark>
-              <SliderMark value={3} {...labelStyles} ml="-1">
-                Large
-              </SliderMark>
-              <SliderMark
-                value={sizeValue}
-                textAlign='center'
-                bg='blue.500'
-                color='white'
-                mt='-10'
-                ml='-5'
-                w='20'   
-                h='25'   
-                borderRadius='5px' 
+        <Box mt={4} mb={8}>
+          <Text>Size</Text>
+          <Box>
+            {sizeRanges.map((size) => (
+              <Button
+                key={size}
+                onClick={() => handleSizeChange(size)}
+                bg={selectedSize === size ? 'blue.500' : 'gray.200'}
+                color={selectedSize === size ? 'white' : 'black'}
+                mr={4}
               >
-                {sizeRanges[sizeValue - 1]}
-              </SliderMark>
-              <SliderTrack>
-                <SliderFilledTrack />
-              </SliderTrack>
-              <SliderThumb />
-            </Slider>
+                {size}
+              </Button>
+            ))}
           </Box>
         </Box>
         <Box mb={10}>
           <Text mb={8}>Age</Text>
           <Box pr={8}>
-            <Slider
+            <RangeSlider
               aria-label='age-slider'
-              min={0}
-              max={15}
+              min={ageMin}
+              max={ageMax}
               step={1}
               value={ageValue}
               onChange={handleAgeChange}
@@ -127,45 +110,56 @@ const FilterMenu = ({ onSelectAge, onSelectSize, onSelectPrice, onSelectPet }) =
               mr={4} // Add mr={4} for right padding
               width="calc(100% - 40px)" // Adjust width for equal length
             >
-              <SliderMark value={0} {...labelStyles}>
-                0
-              </SliderMark>
-              <SliderMark value={5} {...labelStyles}>
-                5
-              </SliderMark>
-              <SliderMark value={10} {...labelStyles} ml="-1">
-                10
-              </SliderMark>
-              <SliderMark value={15} {...labelStyles} ml="-1">
-                15
-              </SliderMark>
-              <SliderMark
-                value={ageValue}
+              <RangeSliderMark value={ageMin} {...labelStyles}>
+                {ageMin}
+              </RangeSliderMark>
+              <RangeSliderMark value={Math.floor((ageMax - ageMin) / 2) + ageMin} {...labelStyles}>
+                {Math.floor((ageMax - ageMin) / 2) + ageMin}
+              </RangeSliderMark>
+              <RangeSliderMark value={ageMax} {...labelStyles} ml="-1">
+                {ageMax}
+              </RangeSliderMark>
+              <RangeSliderMark
+                value={ageValue[0]}
                 textAlign='center'
                 bg='blue.500'
                 color='white'
                 mt='-10'
                 ml='-5'
-                w='20'   
-                h='25'   
-                borderRadius='5px' 
+                w='20'
+                h='25'
+                borderRadius='5px'
               >
-                {ageValue}
-              </SliderMark>
-              <SliderTrack>
-                <SliderFilledTrack />
-              </SliderTrack>
-              <SliderThumb />
-            </Slider>
+                {ageValue[0]}
+              </RangeSliderMark>
+              <RangeSliderMark
+                value={ageValue[1]}
+                textAlign='center'
+                bg='blue.500'
+                color='white'
+                mt='-10'
+                ml='-5'
+                w='20'
+                h='25'
+                borderRadius='5px'
+              >
+                {ageValue[1]}
+              </RangeSliderMark>
+              <RangeSliderTrack>
+                <RangeSliderFilledTrack />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+              <RangeSliderThumb index={1} />
+            </RangeSlider>
           </Box>
         </Box>
         <Box>
           <Text mb={8}>Price</Text>
           <Box pr={8}>
-            <Slider
+            <RangeSlider
               aria-label='price-slider'
-              min={0}
-              max={5000}
+              min={priceMin}
+              max={priceMax}
               step={100}
               value={priceValue}
               onChange={handlePriceChange}
@@ -173,93 +167,142 @@ const FilterMenu = ({ onSelectAge, onSelectSize, onSelectPrice, onSelectPet }) =
               mr={4} // Add mr={4} for right padding
               width="calc(100% - 40px)" // Adjust width for equal length
             >
-              <SliderMark value={0} {...labelStyles}>
-                $0
-              </SliderMark>
-              <SliderMark value={1000} {...labelStyles}>
-                $1,000
-              </SliderMark>
-              <SliderMark value={2000} {...labelStyles} ml="-1">
-                $2,000
-              </SliderMark>
-              <SliderMark value={3000} {...labelStyles} ml="-1">
-                $3,000
-              </SliderMark>
-              <SliderMark value={4000} {...labelStyles} ml="-1">
-                $4,000
-              </SliderMark>
-              <SliderMark value={5000} {...labelStyles} ml="-1">
-                $5,000
-              </SliderMark>
-              <SliderMark
-                value={priceValue}
+              <RangeSliderMark value={priceMin} {...labelStyles}>
+                ${priceMin}
+              </RangeSliderMark>
+              <RangeSliderMark value={Math.floor((priceMax - priceMin) / 5) + priceMin} {...labelStyles}>
+                ${Math.floor((priceMax - priceMin) / 5) + priceMin}
+              </RangeSliderMark>
+              <RangeSliderMark value={Math.floor((priceMax - priceMin) * 2 / 5) + priceMin} {...labelStyles} ml="-1">
+                ${Math.floor((priceMax - priceMin) * 2 / 5) + priceMin}
+              </RangeSliderMark>
+              <RangeSliderMark value={Math.floor((priceMax - priceMin) * 3 / 5) + priceMin} {...labelStyles} ml="-1">
+                ${Math.floor((priceMax - priceMin) * 3 / 5) + priceMin}
+              </RangeSliderMark>
+              <RangeSliderMark value={Math.floor((priceMax - priceMin) * 4 / 5) + priceMin} {...labelStyles} ml="-1">
+                ${Math.floor((priceMax - priceMin) * 4 / 5) + priceMin}
+              </RangeSliderMark>
+              <RangeSliderMark value={priceMax} {...labelStyles} ml="-1">
+                ${priceMax}
+              </RangeSliderMark>
+              <RangeSliderMark
+                value={priceValue[0]}
                 textAlign='center'
                 bg='blue.500'
                 color='white'
                 mt='-10'
                 ml='-5'
-                w='20'   
-                h='25'   
-                borderRadius='5px' 
+                w='20'
+                h='25'
+                borderRadius='5px'
               >
-                ${priceValue}
-              </SliderMark>
-              <SliderTrack>
-                <SliderFilledTrack />
-              </SliderTrack>
-              <SliderThumb />
-            </Slider>
+                ${priceValue[0]}
+              </RangeSliderMark>
+              <RangeSliderMark
+                value={priceValue[1]}
+                textAlign='center'
+                bg='blue.500'
+                color='white'
+                mt='-10'
+                ml='-5'
+                w='20'
+                h='25'
+                borderRadius='5px'
+              >
+                ${priceValue[1]}
+              </RangeSliderMark>
+              <RangeSliderTrack>
+                <RangeSliderFilledTrack />
+              </RangeSliderTrack>
+              <RangeSliderThumb index={0} />
+              <RangeSliderThumb index={1} />
+            </RangeSlider>
           </Box>
         </Box>
       </Box>
     </>
-  )
+  );
 }
 
-const App = () => {
-  const { isOpen, onOpen, onClose } = useDisclosure()
-  const menuRef = useRef()
-  const [selectedAge, setSelectedAge] = useState(18) // State to hold the selected age
-  const [selectedSize, setSelectedSize] = useState(2) // State to hold the selected size
-  const [selectedPriceRange, setSelectedPriceRange] = useState(5000); // State to hold the selected price range
-  const [selectedPet, setSelectedPet] = useState('Dog'); // State to hold the selected pet
+const App = ({ applyFilters }) => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const menuRef = useRef();
+  const [selectedAge, setSelectedAge] = useState([0, 20]); // State to hold the selected age
+  const [selectedSize, setSelectedSize] = useState(null); // State to hold the selected size
+  const [selectedPriceRange, setSelectedPriceRange] = useState([0, 5000]); // State to hold the selected price range
+  const [selectedPet, setSelectedPet] = useState(null); // State to hold the selected pet
+  const toast = useToast();
 
   useOutsideClick({
     ref: menuRef,
     handler: onClose,
-  })
+  });
 
-  const handleSaveChanges = () => {
-    console.log("Saved Size:", selectedSize) // Log the saved size
-    console.log("Saved Age:", selectedAge) // Log the saved age
+  const handleSaveChanges = async () => {
+    console.log("Saved Size:", selectedSize); // Log the saved size
+    console.log("Saved Age:", selectedAge); // Log the saved age
     console.log("Saved Price Range:", selectedPriceRange); // Log the saved price range
     console.log("Saved Pet:", selectedPet); // Log the saved pet
-    onClose();
-  }
+    let query = `min_age=${selectedAge[0]}&max_age=${selectedAge[1]}&min_price=${selectedPriceRange[0]}&max_price=${selectedPriceRange[1]}`;
+    if (selectedSize) {
+      query += `&pet_size=${selectedSize}`;
+    }
+    if (selectedPet) {
+      query += `&pet_type=${selectedPet}`;
+    }
+    console.log(query)
+    try {
+      const response = await axios.get(`http://localhost:3001/api/filterPets?${query}`);
+      const petIDs = response.data.map(pet => pet.pet_id);
+      applyFilters(petIDs); // Pass petIDs to parent component
+      onClose(); // Close the filter menu
+      console.log(petIDs)
+    } catch (error) {
+      console.error("Error fetching filtered pets:", error);
+      toast({
+        title: 'No Such Pets',
+        description: 'Try being a little less picky ;)',
+        status: 'error',
+        duration: 5000,
+        isClosable: true,
+      });
+    }
+  };
+
+  const handleClearFilters = () => {
+    setSelectedAge([0, 20]);
+    setSelectedSize(null);
+    setSelectedPriceRange([0, 5000]);
+    setSelectedPet(null);
+  };
 
   return (
-    <>
-<Box p={6}>
-  <Menu isOpen={isOpen}>
-    <MenuButton as={Button} onClick={isOpen ? onClose : onOpen}>
-      Filter
-    </MenuButton>
-    <MenuList ref={menuRef} p={4} minW="100vh" h="75vh" position="relative" >
-      <FilterMenu 
-        onSelectAge={setSelectedAge} 
-        onSelectSize={setSelectedSize} 
-        onSelectPrice={setSelectedPriceRange} 
-        onSelectPet={setSelectedPet} 
-      />
-      <Box position="absolute" right="4" bottom="1">
-        <Button onClick={handleSaveChanges}>Save Changes</Button> {/* Button to save changes */}
-      </Box>
-    </MenuList>
-  </Menu>
-</Box>
+    <Box p={6}>
+      <Menu isOpen={isOpen}>
+        <MenuButton as={Button} onClick={isOpen ? onClose : onOpen}>
+          Filters
+        </MenuButton>
+        <MenuList ref={menuRef} p={4} minW="100vh" h="75vh" position="relative">
+          <FilterMenu
+            onSelectAge={setSelectedAge}
+            onSelectSize={setSelectedSize}
+            onSelectPrice={setSelectedPriceRange}
+            onSelectPet={setSelectedPet}
+            selectedPet={selectedPet}
+            selectedSize={selectedSize} // Pass selectedSize state to FilterMenu
+            ageMin={0}
+            ageMax={20}
+            priceMin={0}
+            priceMax={5000}
+          />
+          <Box position="absolute" right="4" bottom="1">
+            <Button onClick={handleSaveChanges} mr={4}>Show me the pets!</Button>
+            <Button onClick={handleClearFilters}>Clear Filters</Button>
+          </Box>
+        </MenuList>
+      </Menu>
+    </Box>
+  );
+};
 
-    </>
-  )
-}
-
-export default App
+export default App;

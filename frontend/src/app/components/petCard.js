1482afbad6c9c057d5f326bbec10f1b95ca76366
raellusionz <146325019+raellusionz@users.noreply.github.com>
2024-07-17 22:55:33 +0800

    Pet displaying

--- frontend/src/app/components/petCard.js
+++ frontend/src/app/components/petCard.js
@@ -41,11 +41,11 @@ const PetCard = ({ pet, onClick }) => {
       <Box>
         <Box d="flex" alignItems="baseline" mt="10px">
           <Text fontWeight="semibold" as="h4" lineHeight="tight" isTruncated textAlign="center">
-            {pet.name}
+            {pet.pet_name}
           </Text>
         </Box>
         <Text mt={1} color="gray.500" noOfLines={2} textAlign="center" fontSize={["0.65rem", "0.70rem", "0.75rem", "0.75rem"]} >
-          {pet.description}
+          {pet.pet_breed}
         </Text>
       </Box>
     </Box>

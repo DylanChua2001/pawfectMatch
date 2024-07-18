fbaffaed59e372107ae30313b672a875dc414ca8
DylanChua2001 <dylanchua2001dc@gmail.com>
2024-07-18 22:20:16 +0800

    edited allow cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
-    'http://0.0.0.0'
+    'http://0.0.0.0',
+    'https://pawfect-match-backend-six.vercel.app/'
 ]
 
 module.exports = allowedOrigins

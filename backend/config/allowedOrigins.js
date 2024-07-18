8ac9b4a49614bbfab3d99aa0d0c67424a7a0138f
DylanChua2001 <134465251+DylanChua2001@users.noreply.github.com>
2024-07-18 22:27:07 +0800

    Dylan (#49)
    * Deleted login folder, made default page login page
    
    * Created header.js component, as well as component folder, edited pets page to import header
    
    * removed checkbox
    
    * moves the pages into a pages folder
    
    * Edited the routing to the correct pages, created a filter component and wrapped layout.js in the chakra provider
    
    * removed background.css
    
    * added background to pets page, will do so for others after
    
    * fixed hydration error by wrapping it in <></>
    
    * LLM v2 (sql data + postgres data store)
    
    * fixed exposed postgres db
    
    * created filter component, with a save change button that will save the size as a constant.
    
    * added age range filter slider component
    
    * added price range slider, edited margins etc
    
    * Added background and header to selection page. Added labels to filter
    
    * Added pet selection button
    
    * added function to close filter on save
    
    * Added pets image, created new petCard and petList component, added them to page
    
    * edited the formatting of the petcards and petlist to allow it all to squeeze into one line
    
    * Added scrollability function to the pets page, and made some margin adjustments
    
    * adjusted the pet card text and background
    
    * added pet profile page with component and images
    
    * Created a tempdata folder and pets.json to import data, added some fake dummy data. Pets page will import from there instead of a constant
    
    * Removed petprofile specific page, and made usestate to display data of each page dynamically
    
    * Adjusted margins and made the back to list a button
    
    * Added like/favorite button, installed icon package for heart shape
    
    * Added routing menu to the avatar icon
    
    * added search function to pet list and installed more icon packages
    
    * added profile page component and page.
    
    FOR RAE: Added strip page and button
    
    * llm v2.0 (more user character focused)
    
    * fixed hydration error
    
    * adding chat websocket backend
    
    * updated fe packages
    
    * fe fix
    
    * fe changes
    
    * chatbot integration
    
    * chat fe, be and integration v1.0
    
    * frontend fixes
    
    * installed express-session
    
    * session (not fixed yet)
    
    * login authentication
    
    * cookies fixed
    
    * integrated profile page
    
    * integrated hlaf of the chat page
    
    * fixed integration of chat page partially
    
    * integrated login, profile, chat page
    
    * filter and pet list integrated
    
    * register page and moving of pages
    
    * admin access for pets
    
    * fix login and header css
    
    * fix merging 16/7/24
    
    * show picture for pet card/header
    
    * add training package works and increased auth for other pages
    
    * cleaned up header and training package
    
    * deployment v1.0
    
    * backend deployment v1.1
    
    * backend deployment v1.2
    
    * deployment final v1.0
    
    * edited allow cors
    
    * deployment linking v1.1
    
    ---------
    
    Co-authored-by: jong515 <138593509+jong515@users.noreply.github.com>
    Co-authored-by: shyancham <shyancham@users.noreply.github.com>

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://0.0.0.0',
-    'https://pawfect-match-backend-six.vercel.app/'
+    'https://pawfect-match-three.vercel.app/'
 ]
 
 module.exports = allowedOrigins
